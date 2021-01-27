d53aca3373ee38f578788ba9d025d3d3f4015bf3
Robin <robinslot@hotmail.nl>
2021-01-27 15:43:16 +0100

    Remove unnecessary build arg

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ set -e
 VERSION=1.1
 ./build.sh
 docker build -t namelessmc/discord-link:$VERSION --build-arg LINK_VERSION=${VERSION} .
-docker build -t namelessmc/discord-link-postgres:$VERSION --build-arg LINK_VERSION=${VERSION} postgres-docker
+docker build -t namelessmc/discord-link-postgres:$VERSION postgres-docker
 docker push namelessmc/discord-link:$VERSION
 docker push namelessmc/discord-link-postgres:$VERSION
