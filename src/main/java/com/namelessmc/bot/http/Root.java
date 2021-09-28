b477ab24658afc626d67c533bca1e4c03eeeaddb
Robin <robin@derkad.es>
2021-09-28 17:36:42 +0200

    count guilds in all shards

--- src/main/java/com/namelessmc/bot/http/Root.java
+++ src/main/java/com/namelessmc/bot/http/Root.java
@@ -1,6 +1,7 @@
 package com.namelessmc.bot.http;
 
 import java.io.IOException;
+import java.util.stream.IntStream;
 
 import com.namelessmc.bot.Main;
 import com.namelessmc.bot.connections.BackendStorageException;
@@ -8,6 +9,8 @@ import com.namelessmc.bot.connections.BackendStorageException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.utils.cache.SnowflakeCacheView;
 
 public class Root extends HttpServlet {
 
@@ -19,7 +22,11 @@ public class Root extends HttpServlet {
 		response.getWriter().println("<p>This is a <a href=\"https://github.com/NamelessMC/Nameless-Link\">Nameless-Link</a> instance.</p>");
 		response.getWriter().println("<p>If you run this bot, congratulations! It is working properly. If you are a regular person, there's nothing you can do here. Continue your day with a sense of pride you saw this page.</p>");
 		try {
-			final long guildCount = Main.getJda(0).getGuildCache().size();
+			long guildCount = IntStream.range(0, Main.getShardCount())
+					.mapToObj(Main::getJda)
+					.map(JDA::getGuildCache)
+					.mapToLong(SnowflakeCacheView::size)
+					.sum();
 			final long connectionCount = Main.getConnectionManager().countConnections();
 			response.getWriter().println("<p>This bot is in " + guildCount + " guilds, connected to " + connectionCount + " websites.");
 		} catch (final BackendStorageException e) {
