6b9efd653583e776fde5109b0e1b727a55d40bfe
Robin <robinslot@hotmail.nl>
2021-10-10 19:57:56 +0200

    Remove command_prefix column

--- postgres-docker/db.sql
+++ postgres-docker/db.sql
@@ -2,6 +2,5 @@ CREATE TABLE connections (
   id SERIAL PRIMARY KEY,
   guild_id BIGINT UNIQUE NOT NULL,
   api_url TEXT UNIQUE NOT NULL,
-  last_use BIGINT NOT NULL,
-  command_prefix TEXT DEFAULT NULL
+  last_use BIGINT NOT NULL
 );
