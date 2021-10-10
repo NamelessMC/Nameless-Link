04c86a1f9db598e8c45aa21e52a4c72230293974
Robin <robinslot@hotmail.nl>
2021-10-10 19:59:05 +0200

    bump version

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-VERSION=2.0
+VERSION=3
 ./build.sh
 export DOCKER_CLI_EXPERIMENTAL=enabled
 docker buildx build -t namelessmc/discord-link:$VERSION --platform=linux/arm,linux/arm64,linux/amd64 --build-arg LINK_VERSION=${VERSION} . --push
