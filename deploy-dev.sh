7ee5b13c19167abdb0b1fc0a15fcad648a4b7e2e
Robin <robinslot@hotmail.nl>
2021-10-24 23:45:35 +0200

    don't bukld in deploy script

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 VERSION=3
-./build.sh
+# ./build.sh
 docker build -t namelessmc/discord-link:dev --build-arg LINK_VERSION=${VERSION} .
 docker push namelessmc/discord-link:dev
--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 VERSION=3
-./build.sh
+# ./build.sh
 export DOCKER_CLI_EXPERIMENTAL=enabled
 docker buildx build -t namelessmc/discord-link:$VERSION --platform=linux/arm,linux/arm64,linux/amd64 --build-arg LINK_VERSION=${VERSION} . --push
 docker buildx build -t namelessmc/discord-link-postgres:$VERSION --platform=linux/arm,linux/arm64,linux/amd64 postgres-docker --push
