46b7a7a02e1d0d051039b22f8a08b9a69b447215
Robin <robinslot@hotmail.nl>
2021-10-24 23:49:52 +0200

    use buildx for dev

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -2,5 +2,4 @@
 set -e
 VERSION=3
 # ./build.sh
-docker build -t namelessmc/discord-link:dev --build-arg LINK_VERSION=${VERSION} .
-docker push namelessmc/discord-link:dev
+docker buildx build -t namelessmc/discord-link:dev --platform=linux/arm,linux/arm64,linux/amd64 --build-arg LINK_VERSION=${VERSION} . --push
