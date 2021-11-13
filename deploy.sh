07467b82eda90cd9422c1bab0a3714045b32d078
Robin <robinslot@hotmail.nl>
2021-11-13 13:52:18 +0100

    Remove deploy script

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-VERSION=3
-# ./build.sh
-export DOCKER_CLI_EXPERIMENTAL=enabled
-docker buildx build -t namelessmc/discord-link:$VERSION --platform=linux/arm,linux/arm64,linux/amd64 . --push
-docker buildx build -t namelessmc/discord-link-postgres:$VERSION --platform=linux/arm,linux/arm64,linux/amd64 postgres-docker --push
